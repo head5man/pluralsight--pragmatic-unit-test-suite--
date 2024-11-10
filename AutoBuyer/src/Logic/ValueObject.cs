42499f57c534d0247fc86ea45792ee6a98ae40ef
Tuomas Lahtinen <tuomas123lahtinen@gmail.com>
2024-11-10 10:31:11 +0200

    Import ValueObject<T>
    Import and adapt ValueObject<T> concept from [Applying Functional Principles in C# - Vladimir Khorikov](https://app.pluralsight.com/library/courses/csharp-applying-functional-principles)

---
+++ AutoBuyer/src/Logic/ValueObject.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoBuyer.Logic
+{
+        public abstract class ValueObject<T>
+        where T : ValueObject<T>
+    {
+        public override bool Equals(object obj)
+        {
+            var valueObject = obj as T;
+
+            if (ReferenceEquals(valueObject, null))
+                return false;
+
+            return Equals(valueObject);
+        }
+
+        public override int GetHashCode()
+        {
+            return GetHashCode(this);
+        }
+
+        protected abstract bool Equals(T other);
+
+        protected abstract int GetHashCode(ValueObject<T> @object);
+
+        public static bool operator ==(ValueObject<T> a, ValueObject<T> b)
+        {
+            if (ReferenceEquals(a, null) && ReferenceEquals(b, null))
+                return true;
+
+            if (ReferenceEquals(a, null) || ReferenceEquals(b, null))
+                return false;
+
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(ValueObject<T> a, ValueObject<T> b)
+        {
+            return (a == b) is false;
+        }
+    }
+
+}
