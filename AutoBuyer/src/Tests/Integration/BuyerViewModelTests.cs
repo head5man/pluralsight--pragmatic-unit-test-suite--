02e0b02d06203adbff7405804ca507ab7b098355
Tuomas Lahtinen <tuomas123lahtinen@gmail.com>
2024-11-24 16:00:22 +0200

    Refactor BuyerViewModelTests to use real database

--- AutoBuyer/src/Tests/Integration/BuyerViewModelTests.cs
+++ AutoBuyer/src/Tests/Integration/BuyerViewModelTests.cs
@@ -16,56 +16,37 @@ namespace Tests.Integration
         public void Joining_a_sale()
         {
             var connMock = new Mock<IStockItemConnection>();
-            var repoMock = new Mock<IBuyerRepository>();
-            string savedId = null;
-            Buyer savedBuyer = null;
-            repoMock
-                .Setup(x => x.Save(It.IsAny<string>(), It.IsAny<Buyer>()))
-                .Callback<string, Buyer>(
-                (itemId, buyer) =>
-                {
-                    savedId = itemId;
-                    savedBuyer = buyer;
-                });
 
-            var sut = new BuyerViewModel("1234", 1, 2, "buyer", connMock.Object, repoMock.Object);
+            var sut = new BuyerViewModel("1234", 1, 2, "buyer", connMock.Object, new BuyerRepository(Configuration));
 
-            savedId.ShouldEqual("1234");
+            var savedBuyer = DB.GetBuyer("1234");
+
+            savedBuyer.State.ShouldEqual(BuyerState.Joining);
             savedBuyer.MaximumPrice.ShouldEqual(1);
             savedBuyer.NumberToBuy.ShouldEqual(2);
             savedBuyer.BuyerName.ShouldEqual("buyer");
-            savedBuyer.Snapshot
-                .VerifyState(BuyerState.Joining)
-                .VerifyBoughtSoFar(0)
-                .VerifyNumberInStock(0)
-                .VerifyCurrentPrice(0);
+            savedBuyer.BoughtSoFar.ShouldEqual(0);
+            savedBuyer.NumberInStock.ShouldEqual(0);
+            savedBuyer.CurrentPrice.ShouldEqual(0);
         }
 
         [Fact]
         public void Buying_an_item()
         {
             var connMock = new Mock<IStockItemConnection>();
-            var repoMock = new Mock<IBuyerRepository>();
-            string savedId = null;
-            Buyer savedBuyer = null;
-            repoMock
-                .Setup(x => x.Save(It.IsAny<string>(), It.IsAny<Buyer>()))
-                .Callback<string, Buyer>(
-                (itemId, buyer) =>
-                {
-                    savedId = itemId;
-                    savedBuyer = buyer;
-                });
 
-            var sut = new BuyerViewModel("1234", 1, 2, "buyer", connMock.Object, repoMock.Object);
+            var sut = new BuyerViewModel("1234", 1, 2, "buyer", connMock.Object, new BuyerRepository(Configuration));
             connMock.Raise(x => x.MessageReceived += null, "Event: PRICE; CurrentPrice: 1; NumberInStock: 5");
 
-            repoMock.Verify(x => x.Save(savedId, savedBuyer), Times.Exactly(2));
-            savedBuyer.Snapshot
-                .VerifyState(BuyerState.Buying)
-                .VerifyCurrentPrice(1)
-                .VerifyNumberInStock(5)
-                .VerifyBoughtSoFar(0);
+            var savedBuyer = DB.GetBuyer("1234");
+
+            savedBuyer.State.ShouldEqual(BuyerState.Buying);
+            savedBuyer.MaximumPrice.ShouldEqual(1);
+            savedBuyer.NumberToBuy.ShouldEqual(2);
+            savedBuyer.BuyerName.ShouldEqual("buyer");
+            savedBuyer.BoughtSoFar.ShouldEqual(0);
+            savedBuyer.NumberInStock.ShouldEqual(5);
+            savedBuyer.CurrentPrice.ShouldEqual(1);
             connMock.Verify(x => x.SendMessage(StockCommand.Buy(1, 2)));
         }
     }
